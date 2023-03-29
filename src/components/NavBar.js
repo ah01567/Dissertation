dd8488d29c68f00bbd2d22adb20985d26895224b
Ahmed Henine <ah01567@gmail.com>
2023-03-29 19:00:49 +0100

    Added 'course' and 'Policy' to NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -10,11 +10,13 @@ function ColorSchemesExample() {
           <Navbar.Brand href="#home">MyOnlyBook</Navbar.Brand>
             <Nav className="me-auto">
                 <Nav.Link href="/">Home</Nav.Link>
+                <Nav.Link href="/">Course</Nav.Link>
                 <Nav.Link href="/">ChatBox</Nav.Link>
                 <Nav.Link href="/">Progress</Nav.Link>
                 <Nav.Link href="/">Feedback</Nav.Link>
                 <NavDropdown title="User" id="navbarScrollingDropdown">
                     <NavDropdown.Item href="/">Profile settings</NavDropdown.Item>
+                    <NavDropdown.Item href="/">Privacy policy</NavDropdown.Item>
                     <NavDropdown.Item href="/">Contact Us</NavDropdown.Item>
                     <NavDropdown.Divider />
                     <NavDropdown.Item href="/">
