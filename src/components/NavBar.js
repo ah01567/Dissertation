b0295647004f6c2a25bc78a8f6410a64b3d43a0f
Ahmed Henine <ah01567@gmail.com>
2023-03-30 23:22:18 +0100

    changed 'href' to 'to'

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,6 +6,7 @@ import { auth } from '../pages/firebase';
 import { useNavigate } from 'react-router-dom';
 import {  signOut } from "firebase/auth";
 import '../design/Navbar.css';
+import { Link } from 'react-router-dom';
 import { FaHome } from 'react-icons/fa';
 import { FaBook } from "react-icons/fa";
 import { FaRocketchat } from "react-icons/fa";
@@ -29,19 +30,19 @@ function ColorSchemesExample() {
   return (
       <Navbar ClassName="navbar" variant="dark" >
         <Container>
-          <Navbar.Brand href="/" className='NavLogoWithMargin' style={{fontSize: '1.5rem'}}>MyOnlyBook</Navbar.Brand>
+          <Navbar.Brand as={Link} to="/" className='NavLogoWithMargin' style={{fontSize: '1.5rem'}}>MyOnlyBook</Navbar.Brand>
             <Nav className="me-auto">
-                <Nav.Link href="/" className='NavItemWithMargin'> <FaHome size={20}/> Home</Nav.Link>
-                <Nav.Link href="/" className='NavItemWithMargin'> <FaBook/> Course</Nav.Link>
-                <Nav.Link href="/" className='NavItemWithMargin'><FaRocketchat/> ChatBox</Nav.Link>
-                <Nav.Link href="/" className='NavItemWithMargin'> <FaChartLine/> Progress</Nav.Link>
-                <Nav.Link href="/" className='NavItemWithMargin'> <FaWpforms/> Feedback</Nav.Link>
+                <Nav.Link as={Link} to="/" className='NavItemWithMargin'> <FaHome size={20}/> Home</Nav.Link>
+                <Nav.Link as={Link} to="/" className='NavItemWithMargin'> <FaBook/> Course</Nav.Link>
+                <Nav.Link as={Link} to="/" className='NavItemWithMargin'><FaRocketchat/> ChatBox</Nav.Link>
+                <Nav.Link as={Link} to="/" className='NavItemWithMargin'> <FaChartLine/> Progress</Nav.Link>
+                <Nav.Link as={Link} to="/" className='NavItemWithMargin'> <FaWpforms/> Feedback</Nav.Link>
                 <NavDropdown title="User" id="navbarScrollingDropdown" >
-                    <NavDropdown.Item href="/">Profile settings</NavDropdown.Item>
-                    <NavDropdown.Item href="/">Privacy policy</NavDropdown.Item>
-                    <NavDropdown.Item href="/">Contact Us</NavDropdown.Item>
+                    <NavDropdown.Item as={Link} to="/">Profile settings</NavDropdown.Item>
+                    <NavDropdown.Item as={Link} to="/">Privacy policy</NavDropdown.Item>
+                    <NavDropdown.Item as={Link} to="/">Contact Us</NavDropdown.Item>
                     <NavDropdown.Divider />
-                    <NavDropdown.Item href="/" onClick={handleLogout}>
+                    <NavDropdown.Item as={Link} to="/" onClick={handleLogout}>
                         Logout
                     </NavDropdown.Item>
                 </NavDropdown>
