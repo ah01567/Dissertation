3efd36d9f1788d6bdb36ac63dbdc2694fd192cde
Ahmed Henine <ah01567@gmail.com>
2023-03-30 17:55:42 +0100

    Added design to NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,6 +7,10 @@ import { useNavigate } from 'react-router-dom';
 import {  signOut } from "firebase/auth";
 import '../design/Navbar.css';
 import { FaHome } from 'react-icons/fa';
+import { FaBook } from "react-icons/fa";
+import { FaRocketchat } from "react-icons/fa";
+import { FaChartLine } from "react-icons/fa";
+import { FaWpforms } from "react-icons/fa";
 
 function ColorSchemesExample() {
 
@@ -23,16 +27,16 @@ function ColorSchemesExample() {
 }
 
   return (
-      <Navbar bg="dark" variant="dark" >
+      <Navbar ClassName="navbar" variant="dark" >
         <Container>
-          <Navbar.Brand href="#home" className='NavLogoWithMargin'>MyOnlyBook</Navbar.Brand>
+          <Navbar.Brand href="/" className='NavLogoWithMargin' style={{fontSize: '1.5rem'}}>MyOnlyBook</Navbar.Brand>
             <Nav className="me-auto">
                 <Nav.Link href="/" className='NavItemWithMargin'> <FaHome size={20}/> Home</Nav.Link>
-                <Nav.Link href="/" className='NavItemWithMargin'>Course</Nav.Link>
-                <Nav.Link href="/" className='NavItemWithMargin'>ChatBox</Nav.Link>
-                <Nav.Link href="/" className='NavItemWithMargin'>Progress</Nav.Link>
-                <Nav.Link href="/" className='NavItemWithMargin'>Feedback</Nav.Link>
-                <NavDropdown title="User" id="navbarScrollingDropdown">
+                <Nav.Link href="/" className='NavItemWithMargin'> <FaBook/> Course</Nav.Link>
+                <Nav.Link href="/" className='NavItemWithMargin'><FaRocketchat/> ChatBox</Nav.Link>
+                <Nav.Link href="/" className='NavItemWithMargin'> <FaChartLine/> Progress</Nav.Link>
+                <Nav.Link href="/" className='NavItemWithMargin'> <FaWpforms/> Feedback</Nav.Link>
+                <NavDropdown title="User" id="navbarScrollingDropdown" >
                     <NavDropdown.Item href="/">Profile settings</NavDropdown.Item>
                     <NavDropdown.Item href="/">Privacy policy</NavDropdown.Item>
                     <NavDropdown.Item href="/">Contact Us</NavDropdown.Item>
