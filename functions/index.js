02466e984a908ca82a080314d292447f98696cbb
Ahmed Henine <ah01567@gmail.com>
2023-04-03 00:08:47 +0100

    edited firebase cloud functions

--- functions/index.js
+++ functions/index.js
@@ -1,29 +1,16 @@
-const functions = require("firebase-functions");
+const functions = require('firebase-functions');
 const admin = require('firebase-admin');
-// // Create and deploy your first functions
-// // https://firebase.google.com/docs/functions/get-started
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
 
 admin.initializeApp();
 
-exports.checkUserRole = functions.auth.user().onCreate(async (user) => {
-    const userRef = admin.database().ref(`Users/${user.uid}`);
-    const snapshot = await userRef.once('value');
-    const role = snapshot.child('role').val();
-  
-    if (role === 'TEACHER') {
-      await admin.auth().setCustomUserClaims(user.uid, { 
-        admin: true 
-      }).then(() => {
-        return {
-          message: 'Congrats !! You are now an Admin'
-        }
-      }).catch (err => {
-        return err; 
-      });
-    }
-  });
+exports.checkUserRole = functions.database.ref('Users/C29iALoFhFUZg60xeJujX53pS2I3').onWrite(async (change, context) => {
+  const userRef = admin.database().ref(`Users/${context.params.uid}`);
+  const snapshot = await userRef.once('value');
+  const role = snapshot.child('role').val();
+
+  if (role === 'TEACHER') {
+    await admin.auth().setCustomUserClaims(context.params.uid, { admin: true });
+  } else {
+    await admin.auth().setCustomUserClaims(context.params.uid, { admin: false });
+  }
+});
