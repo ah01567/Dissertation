42faff1cdfa23778408872adb3ff91c131887a49
Ahmed Henine <ah01567@gmail.com>
2023-04-08 04:08:27 +0100

    added students to MyStudents DB using email

--- src/pages/MyStudents.js
+++ src/pages/MyStudents.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, usersRef } from 'react';
 import useAuth from "./CurrentUser";
 import '../Design/MyStudents.css';
 import NavBar from '../components/NavBar';
@@ -6,7 +6,7 @@ import Spinner from '../components/Spinner';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import Form from 'react-bootstrap/Form';
-import { getDatabase, ref, onValue } from 'firebase/database';
+import { getDatabase, ref, set, onValue} from 'firebase/database';
 
 const MyStudents = () => {
   const { currentUser, isAdmin, firebaseInitialized } = useAuth();
@@ -17,13 +17,20 @@ const MyStudents = () => {
   }
 
   const handleAddStudent = () => {
+    //const [studentId, setstudentId] = useState('');
     const email = document.getElementById('student-email').value;
     const dbRef = ref(getDatabase(), 'Users');
     onValue(dbRef, (snapshot) => {
       const users = snapshot.val();
       const matchingUser = Object.values(users).find(user => user.email === email);
+      const studentId = Object.keys(users).find(key => users[key] === matchingUser);
+      const teacherId = currentUser.uid; 
+      const MystudentsDB = ref(getDatabase(), `MyStudents/${teacherId}/${studentId}`);
+      const studentData = {fname:matchingUser.fname, lname:matchingUser.lname};
       if (matchingUser) {
-        setStudents(prevStudents => [...prevStudents, matchingUser]);
+        {isAdmin && 
+        set(MystudentsDB,studentData );
+        setStudents(prevStudents => [...prevStudents, matchingUser]); }
       }
     });
   }
@@ -38,14 +45,14 @@ const MyStudents = () => {
             <Card.Text>Enter your students' <b>Emails</b> and add them to your MyStudents List, using the search bar below:</Card.Text>
             <h3 className='mystudents-search-title'>MyStudent search: </h3>
             <div className='mystudents-search-title'>
-              <Form.Control id="student-email" type="text" placeholder="Enter your student's Email address here" style={{width: '60%'}}/>
+              <Form.Control id="student-email" type="email" placeholder="Enter your student's Email address here" style={{width: '60%'}}/>
             </div> <br/>
             <div className='all'>
             {students.length > 0 && (
             <div className="my-students-list">
                 <ul>
                 {students.map(student => (
-                    <p className='mystudents-list-item' key={student.uid}>{student.email}</p>
+                    <p className='mystudents-list-item' key={student.uid}>{student.fname} {student.lname}</p>
                 ))}
                 </ul>
             </div>
