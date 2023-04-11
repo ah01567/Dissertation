e1fbc646a8f1b098ca6b023de9c67fa7b6aa97a8
Ahmed Henine <ah01567@gmail.com>
2023-04-11 23:51:06 +0100

    Added Progress Page design+ Table for each module

--- src/pages/progress.js
+++ src/pages/progress.js
@@ -1,19 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import useAuth from "./CurrentUser";
-import '../Design/MyStudents.css';
+import { getDatabase, onValue, ref } from 'firebase/database';
 import NavBar from '../components/NavBar';
 import Spinner from '../components/Spinner';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import Form from 'react-bootstrap/Form';
+import Table from 'react-bootstrap/Table';
+import '../Design/progress.css';
 
 const MyStudents = () => {
-  const {firebaseInitialized } = useAuth();
+  const { currentUser, firebaseInitialized } = useAuth();
+  const [myModules, setMyModules] = useState([]);
+
+  useEffect(() => {
+    if (currentUser) {
+      const teacherModulesRef = ref(getDatabase(), `TeacherModules/${currentUser.uid}`);
+      onValue(teacherModulesRef, (snapshot) => {
+        const data = snapshot.val();
+        if (data) {
+          const modules = Object.keys(data).map((key) => data[key]);
+          setMyModules(modules);
+        }
+      });
+    }
+  }, [currentUser]);
 
   if (!firebaseInitialized) {
     return <Spinner />;
   }
-  
+
   return (
     <div>
       <NavBar />
@@ -29,6 +45,44 @@ const MyStudents = () => {
             <div className='add-btn'><Button  variant="success">display student results</Button></div>
           </Card.Body>
         </Card>
+
+        {myModules.map((module) => (
+          <Card className='card' key={module.id}>
+            <Card.Header as="h3" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>{module.title}</Card.Header>
+            <Card.Body>
+            <Table bordered style={{ maxWidth: "60%"}}>
+              <thead>
+                <tr style={{ maxWidth: "30%", backgroundColor: 'lightgrey' }}>
+                  <th style={{ backgroundColor: 'white' }}></th>
+                  <th>Semester 01</th>
+                  <th>Semester 02</th>
+                  <th>Semester 03</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td style={{ backgroundColor: 'lightgrey' }}>Test 01</td>
+                  <td>Mark</td>
+                  <td>Otto</td>
+                  <td>@mdo</td>
+                </tr>
+                <tr>
+                  <td style={{ backgroundColor: 'lightgrey' }}>Test 02</td>
+                  <td>Jacob</td>
+                  <td>Thornton</td>
+                  <td>@fat</td>
+                </tr>
+                <tr>
+                  <td style={{ backgroundColor: 'lightgrey' }}>Exam</td>
+                  <td>Larry the Bird</td>
+                  <td>Larry the Bird</td>
+                  <td>@twitter</td>
+                </tr>
+              </tbody>
+            </Table>
+            </Card.Body>
+          </Card>
+        ))}
       </div>
     </div>
   );
