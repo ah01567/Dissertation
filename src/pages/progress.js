7e7267e101aab69272dd8e034f6833edf374d35e
Ahmed Henine <ah01567@gmail.com>
2023-04-13 19:37:30 +0100

    Initial push and fetch for students results

--- src/pages/progress.js
+++ src/pages/progress.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import useAuth from "./CurrentUser";
-import { getDatabase, onValue, ref } from 'firebase/database';
+import { getDatabase, onValue, ref, set } from 'firebase/database';
 import NavBar from '../components/NavBar';
 import Spinner from '../components/Spinner';
 import Button from 'react-bootstrap/Button';
@@ -13,8 +13,11 @@ import '../Design/progress.css';
 
 const MyStudents = () => {
   const { currentUser, isAdmin, firebaseInitialized } = useAuth();
-  const [myModules, setMyModules] = useState([]);
-  const [isEditing, setIsEditing] = useState(false);
+
+  const [fname, setFname] = useState('');
+  const [lname, setLname] = useState('');
+  const [studentID, setStudentID] = useState('');
+  const [studentDisplayedName, setStudentDisplayedName] = useState('');
 
   const [test1Semester1, setTest1Semester1] = useState('0');
   const [test1Semester2, setTest1Semester2] = useState('0');
@@ -26,19 +29,14 @@ const MyStudents = () => {
   const [examSemester2, setExamSemester2] = useState('0');
   const [examSemester3, setExamSemester3] = useState('0');
 
+  const [myModules, setMyModules] = useState([]);
+  const [isEditing, setIsEditing] = useState(false);
+
   const data = [
     { name: 'Semester 01', test1: test1Semester1, test2: test2Semester1, exam: examSemester1 },
     { name: 'Semester 02', test1: test1Semester2, test2: test2Semester2, exam: examSemester2 },
     { name: 'Semester 03', test1: test1Semester3, test2: test2Semester3, exam: examSemester3 }
     ];
-    
-    const handleEditClick = () => {
-      setIsEditing(true);
-    };
-
-    const handleSaveClick = () => {
-      setIsEditing(false);
-    };
 
     //Display a Result table/Chart for each module
     useEffect(() => {
@@ -54,6 +52,53 @@ const MyStudents = () => {
       }
     }, [currentUser]);
 
+    // Search if user exists on MyStudents list. Only give fetch results permission to authorized teachers
+    const searchForStudent = () => {
+      const teacherID = currentUser.uid;
+      const mystudentsDB = ref(getDatabase(), `MyStudents/${teacherID}`);
+      onValue(mystudentsDB, (snapshot) => {
+        if (snapshot.exists()) {
+          snapshot.forEach((uidSnapshot) => {
+            const uid = uidSnapshot.key;
+            const childData = uidSnapshot.val();
+            if (childData.fname === fname && childData.lname === lname) {
+              setStudentID(uid);
+              const resultsDB = ref(getDatabase(), `Results/${uid}`);
+              onValue(resultsDB, (snapshot) => {
+                if (snapshot.exists()) {
+                  setStudentDisplayedName('<Student Name> results:');
+                  setTest1Semester1(snapshot.t1s1);
+                  setTest2Semester1(snapshot.t2s1);
+                  setExamSemester1(snapshot.es1);
+                } else {
+                  setStudentDisplayedName('<Student Name> results:');
+                }
+              })
+
+            } else {
+              alert('Sorry! You are not authorized to access this students result');
+              setStudentID('');
+            }
+          })
+        }
+    });
+  }
+
+    const handleEditClick = () => {
+      setIsEditing(true);
+    };
+
+    const handleSaveClick = () => {
+      setIsEditing(false);
+      const resultsDB = ref(getDatabase(), `Results/${studentID}`);
+      const userResults = {
+        t1s1: test1Semester1,
+        t2s1: test2Semester1,
+        es1: examSemester1,
+      }
+      set(resultsDB, userResults);
+    };
+
   if (!firebaseInitialized) {
     return <Spinner />;
   }
@@ -66,15 +111,17 @@ const MyStudents = () => {
         <Card className='search-card'>
           <Card.Header as="h3">MyStudents results:</Card.Header>
           <Card.Body>
-            <Card.Text>Enter your students' <b>Emails</b> to add, edit and save their modules results, using the search bar below:</Card.Text>
+            <Card.Text>Enter your students' <b>First </b>and<b> Last name</b> to add, edit and save their modules results, using the search bar below:</Card.Text>
             <h3 className='mystudents-search-title'>MyStudent search: </h3>
             <div className='mystudents-search-title'>
-              <Form.Control id="student-email" type="email" placeholder="Enter your student's Email address here" style={{width: '60%'}}/>
+              <Form.Control type="text" placeholder="First name" style={{width: '30%', marginRight: '4px'}} value={fname} onChange={(e) => setFname(e.target.value)}/>
+              <Form.Control type="text" placeholder="Last name" style={{width: '30%', marginLeft: '4px'}} value={lname} onChange={(e) => setLname(e.target.value)}/>
             </div> <br/>
-            <div className='add-btn'><Button  variant="success">display student results</Button></div>
+            <div className='add-btn'><Button  variant="success" onClick={searchForStudent}>display student results</Button></div>
           </Card.Body>
         </Card> 
         }
+        <div> {studentDisplayedName} </div>
 
         {myModules.map((module) => (
           <Card className='table' key={module.id}>
