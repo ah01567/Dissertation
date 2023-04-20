cd635ade26be2538cb262c43d2fe9716ba038d8c
Ahmed Henine <ah01567@gmail.com>
2023-04-20 16:52:36 +0100

    Added basic style for 'FriendsList'

--- src/components/chat/FriendsList.js
+++ src/components/chat/FriendsList.js
@@ -1,13 +1,21 @@
 import React from 'react';
-import { ListGroup } from 'react-bootstrap';
+import { ListGroup, Media } from 'react-bootstrap';
 import '/Users/ahmedhenine/Desktop/myonlybook/src/Design/Chatbox.css';
 
 const FriendsList = () => {
   return (
     <ListGroup className="friends-list" style={{marginBottom:'20px'}}>
-      <ListGroup.Item>Friend 1</ListGroup.Item>
-      <ListGroup.Item>Friend 2</ListGroup.Item>
-      <ListGroup.Item>Friend 3</ListGroup.Item>
+      <ListGroup.Item>
+        <div className="d-flex align-items-center">
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT8IxW0B9TzuWakKREpBVikX0Jbi4ahnAcfMdXedONE_w&s"
+            alt="User profile"
+            className="mr-3 rounded-circle"
+            style={{ width: '40px', height: '40px' }}
+          />
+            <div className="flex-grow-1">Charlot Greenwood</div>
+          </div>
+        </ListGroup.Item>
     </ListGroup>
   );
 };
