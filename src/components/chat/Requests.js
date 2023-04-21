d7400907b42ac2b9273736a11777cdb80dced3ea
Ahmed Henine <ah01567@gmail.com>
2023-04-21 18:21:26 +0100

    Accept & Refuse buttons working as intended

--- src/components/chat/Requests.js
+++ src/components/chat/Requests.js
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import useAuth from "/Users/ahmedhenine/Desktop/myonlybook/src/pages/CurrentUser.js";
-import { ref, onValue, get } from "firebase/database";
+import { ref, onValue, get, set, remove } from "firebase/database";
 import { db } from '/Users/ahmedhenine/Desktop/myonlybook/src/pages/firebase.js';
 import { ListGroup, Button } from "react-bootstrap";
 
@@ -19,7 +19,7 @@ const Requests = () => {
             const userRef = ref(db, `Users/${uid}`);
             const userSnapshot = await get(userRef);
             const userData = userSnapshot.val();
-            return `${userData.fname} ${userData.lname}`;
+            return { id: uid, name: `${userData.fname} ${userData.lname}` };
           })
         );
         setUserNames(userNames);
@@ -29,10 +29,23 @@ const Requests = () => {
     });
   }, [currentUserID]);
 
+  const handleAccept = (id, name) => {
+    const friendRef = ref(db, `Friends/${currentUserID}/${id}`);
+    set(friendRef, { name, status: "accepted" }).then(() => {
+      const requestRef = ref(db, `Requests/${currentUserID}/${id}`);
+      remove(requestRef);
+    });
+  };
+
+  const handleRefuse = (id) => {
+    const requestRef = ref(db, `Requests/${currentUserID}/${id}`);
+    remove(requestRef);
+  };
+
   return (
     <ListGroup className="friends-requests-list">
-      {userNames.map((userName) => (
-        <ListGroup.Item key={userName}>
+      {userNames.map(({ id, name }) => (
+        <ListGroup.Item key={id}>
           <div className="d-flex align-items-center">
             <img
               src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT8IxW0B9TzuWakKREpBVikX0Jbi4ahnAcfMdXedONE_w&s"
@@ -40,15 +53,21 @@ const Requests = () => {
               className="mr-3 rounded-circle"
               style={{ width: "40px", height: "40px" }}
             />
-            <div className="flex-grow-1">{userName}</div>
+            <div className="flex-grow-1">{name}</div>
             <Button
               variant="success"
               className="accept-btn mr-2 px-1"
               size="sm"
+              onClick={() => handleAccept(id, name)}
             >
               Accept
             </Button>
-            <Button variant="danger" className="refuse-btn px-1" size="sm">
+            <Button
+              variant="danger"
+              className="refuse-btn px-1"
+              size="sm"
+              onClick={() => handleRefuse(id)}
+            >
               Refuse
             </Button>
           </div>
@@ -58,4 +77,6 @@ const Requests = () => {
   );
 };
 
-export default Requests; 
+export default Requests;
+
+
