662afa2d2dd1e42f35f50f043586f9eccc1b5cd2
Ahmed Henine <ah01567@gmail.com>
2023-05-15 01:08:23 +0100

    Module title fetched from URL +Add content section

--- src/pages/Module.js
+++ src/pages/Module.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import NavBar from '../components/NavBar';
 import Spinner from '../components/Spinner';
 import useAuth from "./CurrentUser";
@@ -6,27 +6,45 @@ import UploadLesson from '../components/course/UploadLesson';
 import Button from 'react-bootstrap/Button';
 
 const Module = () => {
-    const {isAdmin, firebaseInitialized } = useAuth();
-
+    const { isAdmin, firebaseInitialized } = useAuth();
+    const [moduleName, setModuleName] = useState('');
+    const [contentSections, setContentSections] = useState(1);
+  
+    useEffect(() => {
+      const path = window.location.pathname;
+      const moduleName = decodeURI(path.split('/').pop());
+      setModuleName(moduleName);
+    }, []);
+  
+    const handleAddSection = () => {
+      setContentSections(contentSections + 1);
+    };
+  
     if (!firebaseInitialized) {
-        return <Spinner />;
-      }
- 
-    return(
+      return <Spinner />;
+    }
+  
+    return (
+      <div>
         <div>
-            <div>
-                <NavBar />
-            </div>    
-            <div>
-                <h1>Arbic:</h1> {isAdmin &&
-                <React.Fragment>
-                    <Button variant="danger">Delete module</Button>
-                    <Button variant="success">Add content section</Button>
-                </React.Fragment> }
-                <UploadLesson />
-            </div>
+          <NavBar />
         </div>
-    )
-}
+        <div>
+          <h1>{moduleName}:</h1>
+          {isAdmin && (
+            <React.Fragment>
+              <Button variant="danger">Delete module</Button>
+              <Button variant="success" onClick={handleAddSection}>
+                Add content section
+              </Button>
+            </React.Fragment>
+          )}
+          {Array.from({ length: contentSections }, (_, index) => (
+            <UploadLesson key={index} />
+          ))}
+        </div>
+      </div>
+    );
+  };
  
 export default Module;
